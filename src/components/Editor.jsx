74df38adb272412ea8f3371d10e3b5c24cb9578d
thebenzo <amanvrma5799@gmail.com>
2024-10-30 22:19:12 +0530

    Modify Editor options and add component props

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,13 +1,48 @@
+/* eslint-disable react/prop-types */
 import { Editor } from "@monaco-editor/react";
+import { useEffect, useRef } from "react";
 
-function CodeEditor({ theme }) {
+function CodeEditor({ theme, fontSize, fontLigatures, showMinimap }) {
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor) {
+    editorRef.current = editor;
+  }
+
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.updateOptions({
+        fontSize,
+        fontLigatures,
+        minimap: { enabled: showMinimap },
+      });
+    }
+  }, [fontSize, fontLigatures, showMinimap]);
+
   return (
     <Editor
-      className="border border-light-platinum dark:border-dark-charcoal"
       width="100%"
       height="100vh"
       defaultLanguage="cpp"
       theme={theme}
+      onMount={handleEditorDidMount}
+      options={{
+        fontSize: 16,
+        fontFamily: "monospace",
+        fontLigatures: true,
+        glyphMargin: true,
+        cursorStyle: "line-thin",
+        cursorBlinking: "phase",
+        mouseWheelZoom: true,
+        showUnused: true,
+        minimap: {
+          enabled: false,
+        },
+        scrollbar: {
+          verticalScrollbarSize: 10,
+          horizontalScrollbarSize: 10,
+        },
+      }}
     />
   );
 }
